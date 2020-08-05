a162a6abb448558605978b49fade55a9b324095f
Maciej Mielczarek <maciej.marek.mielczarek@gmail.com>
2020-08-05 14:41:56 +0200

    Pierwsza wersja ekranu startowego z działającym wyjściem.

---
+++ tydzien_02/TicTacToe/TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        private const string welcomeMessage = "This application lets you play Tic-Tac-Toe. What do you want to do?";
+        private static readonly string[] menuOptions =
+        {
+            "1. Start a new game.",
+            "2. Load game.",
+            "3. Exit."
+        };
+        static void Main(string[] args)
+        {
+            int menuChoice;
+            do
+            {
+                Console.WriteLine(welcomeMessage);
+                foreach (string option in menuOptions)
+                {
+                    Console.WriteLine(option);
+                }
+                menuChoice = Int32.Parse(Console.ReadLine());
+                Console.WriteLine($"You have chosen the option number {menuChoice}");
+            }
+            while (menuChoice != 3);
+        }
+    }
+}
