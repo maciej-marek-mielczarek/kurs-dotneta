0121e852bd1657f71749670a058f35addbd92226
Maciej Mielczarek <maciej.marek.mielczarek@gmail.com>
2020-08-07 17:15:28 +0200

    Dodałem funkcje do przechowywania rozwiązań zadań z lekcji 2.7. Zrezygnowałem z testów jednostkowych, bo musiałbym sprawdzać wypisywanie napisów, a nie zwracane wartości.

---
+++ tydzien_02/Homeworks/Logic/Logic/Solutions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Logic
+{
+    public static class Solutions
+    {
+        public static void Exercise13()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise12()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise11()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise10()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise09()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise08()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise07()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise06()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise05()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise04()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise03()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise02()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Exercise01()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
