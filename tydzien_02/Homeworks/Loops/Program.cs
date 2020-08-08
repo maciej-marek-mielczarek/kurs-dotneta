cbe671953986f154420902bedd3e3888276377d1
Maciej Mielczarek <maciej.marek.mielczarek@gmail.com>
2020-08-08 23:20:29 +0200

    t02l08 Nadanie zbiorowi rozwiązań struktury żeby dobrze się je sprawdzało.

---
+++ tydzien_02/Homeworks/Loops/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Which exercise would you like to check? Type 0 to exit.");
+                byte exerciseNumber = byte.Parse(Console.ReadLine());
+                switch (exerciseNumber)
+                {
+                    case 0:
+                        Console.WriteLine("Terminating the program...");
+                        break;
+                    case 1:
+                        Solutions.Ex01();
+                        break;
+                    case 2:
+                        Solutions.Ex02();
+                        break;
+                    case 3:
+                        Solutions.Ex03();
+                        break;
+                    case 4:
+                        Solutions.Ex04();
+                        break;
+                    case 5:
+                        Solutions.Ex05();
+                        break;
+                    case 6:
+                        Solutions.Ex06();
+                        break;
+                    case 7:
+                        Solutions.Ex07();
+                        break;
+                    case 8:
+                        Solutions.Ex08();
+                        break;
+                    case 9:
+                        Solutions.Ex09();
+                        break;
+                    case 10:
+                        Solutions.Ex10();
+                        break;
+                    default:
+                        Console.WriteLine("That is not a valid exercise number.");
+                        break;
+                }
+                if(exerciseNumber == 0)
+                {
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ tydzien_02/Homeworks/Loops/Solutions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Loops
+{
+    public class Solutions
+    {
+        internal static readonly int numberOfSolutions = 10;
+
+        public Solutions()
+        {
+        }
+
+        internal static void Ex01()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex02()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex03()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex04()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex05()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex06()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex07()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex08()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex09()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Ex10()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
