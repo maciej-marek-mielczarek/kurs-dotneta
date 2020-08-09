0a69e53cca36bd7329a51917589a540056b29904
Maciej Mielczarek <maciej.marek.mielczarek@gmail.com>
2020-08-09 11:56:58 +0200

    t02l08z03

--- tydzien_02/Homeworks/Loops/Solutions.cs
+++ tydzien_02/Homeworks/Loops/Solutions.cs
@@ -50,7 +50,33 @@ namespace Loops
 
         internal static void Ex03()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("I shall display consecutive Fibonacci numbers, starting with the 1st. Which one should I end with?"); 
+            Console.WriteLine("The max I will allow is 92.");
+            byte maxIdx = GetByte();//index
+            if(maxIdx > 92)
+            {
+                Console.WriteLine("Let's keep the numbers down to 64 bits.");
+                maxIdx = 92;
+            }
+            ulong next, curr = 1, prev = 0;
+            for (byte idx = 1; idx <= maxIdx; idx++)
+            {
+                Console.WriteLine("F(" + idx + ") = " + curr);
+                if(curr > ulong.MaxValue - prev)
+                {
+                    throw new OverflowException();
+                }
+                //Calculate next number:
+                next = curr + prev;
+                //Move up the index
+                prev = curr;
+                curr = next;
+            }
+        }
+
+        private static byte GetByte()
+        {
+            return byte.Parse(Console.ReadLine());
         }
 
         internal static void Ex04()
