13d4f299992531ed81edb193610ed3a371eb4a2d
Maciej Mielczarek <maciej.marek.mielczarek@gmail.com>
2020-08-09 10:42:18 +0200

    t02l08z01

--- tydzien_02/Homeworks/Loops/Solutions.cs
+++ tydzien_02/Homeworks/Loops/Solutions.cs
@@ -11,7 +11,25 @@ namespace Loops
 
         internal static void Ex01()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("I shall check how many prime numbers there are in the interval 0-100.");
+            byte max = 100;
+            bool[] isComposite = new bool[101];//Erathosteneses Sieve
+            byte primesInRange = 0;
+            for(byte divisor = 2; divisor <= max; ++divisor)
+            {
+                if (!isComposite[divisor])
+                {
+                    primesInRange++;
+                    for (byte checkedNumber = (byte)(divisor + 1); checkedNumber <= max; ++checkedNumber)
+                    {// if a and b are bytes, then a+b actually means (int)a+(int)b
+                        if (!isComposite[checkedNumber] && checkedNumber % divisor == 0)
+                        {
+                            isComposite[checkedNumber] = true;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("There are " + primesInRange + " primes in range 0-" + max);
         }
 
         internal static void Ex02()
