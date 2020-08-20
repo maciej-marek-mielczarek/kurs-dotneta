8cb0199dbafd6df88b3cd8051deeda4f71d6a0c6
Maciej Mielczarek <maciej.marek.mielczarek@gmail.com>
2020-08-20 12:27:36 +0200

    Eksperymenty z Enumami.

---
+++ tydzien_03/EnumCasts/EnumCasts/EnumCasts/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EnumCasts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = A.x;
+            Enum b = a;//works
+            Console.WriteLine(b);
+
+            Dictionary<Enum, string> fff = new Dictionary<Enum, string>() { {A.y, "asd" },{B.r, "wed" },{B.w, "pp" } };
+            Console.WriteLine(fff[B.r]);//also works
+
+            Dictionary<A, string> sss = new Dictionary<A, string>() { {A.x, "123" },{ A.y, "456"},{A.z, "789" } };
+            //Dictionary<Enum, string> pairs = sss;//doesn't work
+            //Console.WriteLine(pairs[A.x]);
+        }
+    }
+    public enum A
+    {
+        x,
+        y,
+        z
+    }
+    public enum B
+    {
+        q,
+        w,
+        e,
+        r
+    }
+}
