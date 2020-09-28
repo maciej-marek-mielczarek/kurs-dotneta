6244f64d2528b368748d5b2b294943c7a03599f7
Maciej Mielczarek <maciej.marek.mielczarek@gmail.com>
2020-09-28 18:20:58 +0200

    Dopisanie testów dla FightManager.CalculateScore.

--- Doppelganger/Doppelganger.Tests/App/Managers/Concrete/FightManagerTests.cs
+++ Doppelganger/Doppelganger.Tests/App/Managers/Concrete/FightManagerTests.cs
@@ -1,4 +1,13 @@
 using System;
+using System.Linq;
+using Doppelganger.App.Helpers.Concrete;
+using Doppelganger.App.Managers.Abstract;
+using Doppelganger.App.Managers.Concrete;
+using Doppelganger.App.Services.Abstract;
+using Doppelganger.App.Services.Concrete;
+using Doppelganger.Domain.Common;
+using Doppelganger.Domain.Entity.Settings;
+using Moq;
 using Xunit;
 
 namespace Doppelganger.Tests.App.Managers.Concrete
@@ -7,14 +16,99 @@ namespace Doppelganger.Tests.App.Managers.Concrete
     {
         //Tests for method CalculateScore
         [Fact]
-        public void CalculateScore_Given_Should()
+        public void CalculateScore_GivenZeroCurrentHPs_ShouldReturn100()
         {
             //Arrange
+            Mock<ICreatureService> mock = new Mock<ICreatureService>();
+            foreach (int id in Enumerable.Range(0,DisplaySettings.NumberOfOpps))
+            {
+                mock.Setup(m => m.GetCreatureCurrentHPById(id)).Returns(0);
+                mock.Setup(m => m.GetCreatureMaxHPById(id)).Returns(10);
+            }
+            IFightManager fightManager = new FightManager(new TextService(Language.English), mock.Object , new UserInput());
+            int expectedScore = 100;
             //Act
+            int score = fightManager.CalculateScore();
             //Assert
-            throw new NotImplementedException();
+            Assert.Equal(expectedScore, score);
         }
         
+        [Fact]
+        public void CalculateScore_GivenLessThan10PercentDamageOnEveryone_ShouldReturnZero()
+        {
+            //Arrange
+            Mock<ICreatureService> mock = new Mock<ICreatureService>();
+            foreach (int id in Enumerable.Range(0,DisplaySettings.NumberOfOpps))
+            {
+                mock.Setup(m => m.GetCreatureCurrentHPById(id)).Returns(90);
+                mock.Setup(m => m.GetCreatureMaxHPById(id)).Returns(99);
+            }
+            IFightManager fightManager = new FightManager(new TextService(Language.English), mock.Object , new UserInput());
+            int expectedScore = 0;
+            //Act
+            int score = fightManager.CalculateScore();
+            //Assert
+            Assert.Equal(expectedScore, score);
+        }
+        
+        [Fact]
+        public void CalculateScore_GivenFullHPs_ShouldReturnZero()
+        {
+            //Arrange
+            Mock<ICreatureService> mock = new Mock<ICreatureService>();
+            foreach (int id in Enumerable.Range(0,DisplaySettings.NumberOfOpps))
+            {
+                mock.Setup(m => m.GetCreatureCurrentHPById(id)).Returns(67);
+                mock.Setup(m => m.GetCreatureMaxHPById(id)).Returns(67);
+            }
+            IFightManager fightManager = new FightManager(new TextService(Language.English), mock.Object , new UserInput());
+            int expectedScore = 0;
+            //Act
+            int score = fightManager.CalculateScore();
+            //Assert
+            Assert.Equal(expectedScore, score);
+        }
+        
+        [Fact]
+        public void CalculateScore_Given20PercentDamageOnOneCreature_ShouldReturn2()
+        {
+            //Arrange
+            Mock<ICreatureService> mock = new Mock<ICreatureService>();
+            mock.Setup(m => m.GetCreatureCurrentHPById(0)).Returns(40);
+            mock.Setup(m => m.GetCreatureMaxHPById(0)).Returns(50);
+            foreach (int id in Enumerable.Range(1,DisplaySettings.NumberOfOpps))
+            {
+                mock.Setup(m => m.GetCreatureCurrentHPById(id)).Returns(99);
+                mock.Setup(m => m.GetCreatureMaxHPById(id)).Returns(99);
+            }
+            IFightManager fightManager = new FightManager(new TextService(Language.English), mock.Object , new UserInput());
+            int expectedScore = 2;
+            //Act
+            int score = fightManager.CalculateScore();
+            //Assert
+            Assert.Equal(expectedScore, score);
+        }
+
+        [Fact]
+        public void CalculateScore_GivenOneDeadCreature_ShouldReturn10()
+        {
+            //Arrange
+            Mock<ICreatureService> mock = new Mock<ICreatureService>();
+            mock.Setup(m => m.GetCreatureCurrentHPById(0)).Returns(0);
+            mock.Setup(m => m.GetCreatureMaxHPById(0)).Returns(15);
+            foreach (int id in Enumerable.Range(1,DisplaySettings.NumberOfOpps))
+            {
+                mock.Setup(m => m.GetCreatureCurrentHPById(id)).Returns(99);
+                mock.Setup(m => m.GetCreatureMaxHPById(id)).Returns(99);
+            }
+            IFightManager fightManager = new FightManager(new TextService(Language.English), mock.Object , new UserInput());
+            int expectedScore = 10;
+            //Act
+            int score = fightManager.CalculateScore();
+            //Assert
+            Assert.Equal(expectedScore, score);
+        }
+
         //Tests for method PickAlly
         [Fact]
         public void PickAlly_Given_Should()
